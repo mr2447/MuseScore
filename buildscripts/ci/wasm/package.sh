e9e6eedf10e5399c915bb860c0637bb44eb0868f
Marvin Ren <mr6803@nyu.edu>
2026-06-10 15:21:50 -0400

    Uncomment artifact copy command in package.sh

--- buildscripts/ci/wasm/package.sh
+++ buildscripts/ci/wasm/package.sh
@@ -26,4 +26,4 @@ ARTIFACTS_DIR=build.artifacts
 BUILD_DIR=build.release
 
 # Nothing is needed at the moment, artifacts appear during the build stage
-#cp -r $BUILD_DIR/public_html $ARTIFACTS_DIR/public_html
+cp -r $BUILD_DIR/public_html $ARTIFACTS_DIR/public_html
